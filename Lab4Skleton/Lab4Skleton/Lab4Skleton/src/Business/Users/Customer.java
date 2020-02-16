d53a40a2a3a0728ce97195936087d1639b5e0d20
shreyas-redij <redij.s@husky.neu.edu>
2020-02-15 20:45:47 -0500

    Merged with Lab4

---
+++ Lab4Skleton/Lab4Skleton/Lab4Skleton/src/Business/Users/Customer.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Users;
+
+import Business.Abstract.User;
+import Business.ProductDirectory;
+import java.util.Date;
+
+/**
+ *
+ * @author AEDSpring2019
+ */
+public class Customer extends User implements Comparable<Customer>{
+    private Date cd;
+
+    public Date getCd() {
+        return cd;
+    }
+    
+    public Customer(String password, String userName,Date cd) {
+        super(password, userName, "CUSTOMER");
+        this.cd = cd;
+    }
+  
+
+    
+
+    @Override
+    public String toString() {
+        return getUserName(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public boolean verify(String password){
+        if(password.equals(getPassword()))
+            return true;
+        return false;
+    }
+    
+    @Override
+    public int compareTo(Customer o) {
+        return o.getUserName().compareTo(this.getUserName());
+    }
+    
+}
+    
+
